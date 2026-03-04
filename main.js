45ad2e79fc6582fde5bb6edda1d59c3dbcadcbc3
Mike-A-M <159654413+Mike-A-M@users.noreply.github.com>
2026-03-04 13:12:18 -0500

    updated cat background

--- main.js
+++ main.js
@@ -1,7 +1,72 @@
-const myButton = document.getElementById("zipcode");
+
+// Get random cat images 
+// https://api.thecatapi.com/v1/images/search
+// use above for one imgage
+// api key = live_8kNZp5dDR4NQRQOEq4UgoF5EejH2W6xfmwSXHVf90Qe9obVlSc8SQk0n5NSWtVZY
+
+window.onload = function () {
+    getCat();
+}
+
+async function getCat () {
+
+const url = `https://api.thecatapi.com/v1/images/search?limit=1`;
+const api_key = "live_8kNZp5dDR4NQRQOEq4UgoF5EejH2W6xfmwSXHVf90Qe9obVlSc8SQk0n5NSWtVZY"
+
+ fetch(url,{headers: {
+      'x-api-key': api_key
+    }})
+ .then((response) => {
+   return response.json();
+ })
+.then((data) => {
+  let imagesData = data;
+  imagesData.map(function(imageData) {
+    
+    //use the url from the image object
+    image = `${imageData.url}`;
+  }
+  )
+})
+document.body.style.background = image;
+}
+
+
+
+
+
 
 // Add an event listener for the 'click' event
 myButton.addEventListener('click', zipCodeToGlobal());
+
+
+// Global Coordinates from Zipcode
+// http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
+// country code = 840 for US, use US
+async function zipCodeToGlobal () {
+    // Retrieve zip code from form
+    const zipcode = document.getElementById("zipcode")
+    const myZipcode = zipcode.value;
+
+    // Fetch coordinates
+    const apiKey = "85977e055851755123d0bcc6806eabf4"
+    const apiURL = "http://api.openweathermap.org/geo/1.0/zip?zip="+  myZipcode + ",US&appid=85977e055851755123d0bcc6806eabf4"
+    const response = await fetch(apiURL)
+    const geo = await response.json();
+
+    // Render on page
+   
+    const longtitude = geo.lon;
+    document.getElementById("longitude").textContent = `${longtitude} Degrees`
+    const latititude = geo.lat;
+    document.getElementById("latitude").textContent = `${latititude} Degrees`
+
+}
+
+
+const myButton = document.getElementById("zipcode");
+
+
 
 
 
@@ -14,24 +79,5 @@ async function pullForcast (zipcode){
   
 }
 
-// Global Coordinates from Zipcode
-// http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
-// country code = 840 for US, use US
-async function zipCodeToGlobal () {
-    alert("Function Fired");
-    const zipcode = document.getElementById("zipcode")
-    const myZipcode = zipcode.value;
 
-    alert(myZipcode);
-    const apiKey = "85977e055851755123d0bcc6806eabf4"
-    const apiURL = "http://api.openweathermap.org/geo/1.0/zip?zip="+  myZipcode + ",US&appid=85977e055851755123d0bcc6806eabf4"
-    const response = await fetch(apiURL)
-    const geo = await response.json();
-    // broken here and down
-    const longtitude = geo.lat;
-    alert(longtitude);
-    const latititude = geo.lon;
-    alert(latititude);
-
-}
 
