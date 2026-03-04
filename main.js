c2914ae433add1895537d0c213dc73d9c8fa572b
Mike-A-M <159654413+Mike-A-M@users.noreply.github.com>
2026-03-04 08:04:53 -0500

    added geo loaction API call

--- main.js
+++ main.js
@@ -12,24 +12,26 @@ myButton.addEventListener('click', zipCodeToGlobal());
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 async function pullForcast (zipcode){
   
-
-
 }
-
-
 
 // Global Coordinates from Zipcode
 // http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
 // country code = 840 for US, use US
 async function zipCodeToGlobal () {
     alert("Function Fired");
-    const zip = document.getElementById("city-input");
+    const zipcode = document.getElementById("zipcode")
+    const myZipcode = zipcode.value;
+
+    alert(myZipcode);
     const apiKey = "85977e055851755123d0bcc6806eabf4"
-    const apiURL = "http://api.openweathermap.org/geo/1.0/zip?zip=46231,US&appid=85977e055851755123d0bcc6806eabf4"
+    const apiURL = "http://api.openweathermap.org/geo/1.0/zip?zip="+  myZipcode + ",US&appid=85977e055851755123d0bcc6806eabf4"
     const response = await fetch(apiURL)
+    const geo = await response.json();
     // broken here and down
-    const longtitude = response.lon;
-    const latititude = response.lat;
-    alert(latititude)
+    const longtitude = geo.lat;
+    alert(longtitude);
+    const latititude = geo.lon;
+    alert(latititude);
 
 }
+
